3aadfb642696ce16d7747118ea349f84a3e04f4c
Roman <roman.pawliw@gmail.com>
2018-02-09 22:01:17 +0200

    delete client secret and id

--- config/index.js
+++ config/index.js
@@ -4,16 +4,16 @@ module.exports = {
   dbUrl: 'mongodb://localhost/test',
 
   'facebookAuth' : {
-    'clientID'      : '139690616842157',
-    'clientSecret'  : '93998a361b88f41fdf30d098a1fd95de', 
+    'clientID'      : 'id',
+    'clientSecret'  : 'secret', 
     'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
     'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields' : ['id', 'email', 'name']
   },
 
   'googleAuth': {
-    'clientID'      : '350901502302-ltu5tk32a2v6b4lu21u2fha1p91ngqdu.apps.googleusercontent.com',
-    'clientSecret'  : '7rUXdF6-k6v4a5ePiCvSZpHr',
+    'clientID'      : 'id',
+    'clientSecret'  : 'secret',
     'callbackURL'   : 'http://localhost:3000/auth/google/oauth2Callback'
   }
 }

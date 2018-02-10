e6224589b108eabe22aa72205db104bc1b480545
Roman <roman.pawliw@gmail.com>
2018-02-10 18:47:05 +0200

    create controllers folder and file

---
+++ controllers/controllers.js
@@ -0,0 +1,56 @@
+const Product = require('../models/product');
+
+exports.getAllProducts = (req, res) => {
+  Product.find({}, (err, products) => {
+    if (err) {
+      return res.status(500).send("There was a problem.");
+    }
+    res.json(products);
+  });
+}
+
+exports.createProduct = (req, res) =>{
+  Product.create({
+    title: req.body.title,
+    price: req.body.price,
+    owner_id: req.userId
+  }, (err, product) => {
+    if (err) { 
+      return res.status(500).send("There was a problem of registering the product.");
+    }
+    res.status(200).send({ product_id: product._id, message: 'Product created' });
+  });
+}
+
+exports.deleteProduct = (req, res) => {
+  Product.remove({ _id: req.params.id, owner_id: req.userId }, (err, doc) => {
+    if (err) {
+      return res.status(500).send("There was a problem of deliting the product.");
+    }
+    
+    if (!doc.n) {
+      return res.send({ message: 'You are not owner of this product' });
+    }
+    res.status(200).send({ message: 'Product was deleted' });
+  });
+}
+
+exports.updateProduct = (req, res) => {
+  Product.findOne({ _id: req.params.id }, (err, product) => {
+    if (err) {
+      return res.status(500).send("There was a problem to find the product.");
+    }
+    
+    if (product.owner_id === req.userId) {
+      Product.findByIdAndUpdate(req.params.id, { $set: { title: req.body.title, price: req.body.price }}, 
+        { new: true }, (err, doc) => {
+          if (err) {
+            return res.status(500).send("There was a problem of updating the product.");
+          }
+          res.status(200).send({ message: 'Product was updated' });
+      });
+    } else {
+      res.send({ message: 'You are not owner of this product' });
+    }
+	});
+}
--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product');
+const controllers = require('../controllers/controllers');
 const verifyToken = require('../services/verifyToken');
 
-router.get('/', (req, res, next) => {
-
-  Product.find({}, (err, products) => {
-    if (err) {
-      return res.status(500).send("There was a problem.");
-    }
-
-    res.json(products);
-  });
-});
-
-router.post('/products', verifyToken, (req, res) => {
-
-  Product.create({
-    title: req.body.title,
-    price: req.body.price,
-    owner_id: req.userId
-  }, (err, product) => {
-    if (err) { 
-      return res.status(500).send("There was a problem of registering the product.");
-    }
-    
-    res.status(200).send({ product_id: product._id, message: 'Product created' });
-  });
-
-});
-
-router.put('/products/:id', verifyToken, (req, res) => {
-
-  Product.findOne({ _id: req.params.id }, (err, product) => {
-    if (err) {
-      return res.status(500).send("There was a problem to find the product.");
-    }
-    
-    if (product.owner_id === req.userId) {
-      Product.findOneAndUpdate({ _id: req.params.id }, { $set:{ title: req.body.title, price: req.body.price }}, 
-        { new: true }, (err, doc) => {
-          if (err) {
-            return res.status(500).send("There was a problem of updating the product.");
-          }
-
-          res.status(200).send({ message: 'Product was updated' });
-      });
-    } 
-    else {
-      res.send({ message: 'You are not owner of this product' });
-    }
-  });
-});
-
-router.delete('/products/:id', verifyToken, (req, res) => {
-  
-  Product.findOne({_id: req.params.id }, (err, product) => {
-    if (err) {
-      return res.status(500).send("There was a problem to find the product.");
-    }
-    
-    if (product.owner_id === req.userId) {
-      Product.findByIdAndRemove(req.params.id, (err, deletedProduct) => {
-
-        if (err) {
-          return res.status(500).send("There was a problem of deliting the product.");
-        }
-    
-        res.status(200).send({ message: 'Product was deleted' });
-    
-      });
-    } 
-    else {
-      res.send({ message: 'You are not owner of this product' });
-    }
-
-  });
-  
-});
+router.get('/', controllers.getAllProducts);
+router.post('/products', verifyToken, controllers.createProduct);
+router.put('/products/:id', verifyToken, controllers.updateProduct);
+router.delete('/products/:id', verifyToken, controllers.deleteProduct);
 
 module.exports = router;
